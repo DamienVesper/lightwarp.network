21becc852293d3575af291686e67351dd4133b12
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-10 00:03:26 +0530

    Update widget.js

--- routes/widget.js
+++ routes/widget.js
@@ -3,7 +3,7 @@ const express = require(`express`);
 const router = express.Router();
 
 router.get(`/tts`, async (req, res) => {res.render(`widgets/tts`, {
-    webfront: process.env.APP_DOMAIN
+    webfront: process.env.URL
 })});
 
 module.exports = router;
