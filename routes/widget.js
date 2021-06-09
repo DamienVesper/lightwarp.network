e0be3b42cdc3c29ea7f8044e962369a16bb892c0
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-09 20:40:27 +0530

    woops

--- routes/widget.js
+++ routes/widget.js
@@ -3,7 +3,7 @@ const express = require(`express`);
 const router = express.Router();
 
 router.get(`/tts`, async (req, res) => {res.render(`widgets/tts`, {
-    webfront: process.env.ENV === `prod` ? `localhost` : `sockets.lightwarp.network`
+    webfront: process.env.ENV === `dev` ? `localhost` : `sockets.lightwarp.network`
 })});
 
 module.exports = router;
