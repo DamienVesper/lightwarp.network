d6a1001d544ce8cf349b208fb4e73b68919a50d2
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-09 20:32:16 +0530

    Using EJS for this

--- routes/widget.js
+++ routes/widget.js
@@ -2,6 +2,8 @@ const express = require(`express`);
 
 const router = express.Router();
 
-router.get(`/tts`, async (req, res) => {res.render(`widgets/tts`)});
+router.get(`/tts`, async (req, res) => {res.render(`widgets/tts`, {
+    webfront: process.env.ENV === `prod` ? `localhost` : `sockets.lightwarp.network`
+})});
 
 module.exports = router;
