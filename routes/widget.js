514a9b8f664b470ba9aa420fe74e228fb5b6e543
Damien Vesper <ldamienvesper@gmail.com>
2021-06-14 18:46:14 -0500

    good

--- routes/widget.js
+++ routes/widget.js
@@ -15,6 +15,8 @@ router.get(`/fake`, async (req, res) => {
 router.post(`/fake`, async (req, res) => {
     const { name, message } = req.body;
     broadcast(`prioritymessage`, name, message);
+
+    res.sendStatus(200).json({ success: `I like em big` });
 });
 
 module.exports = router;
